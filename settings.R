42d3186cd1ae705398ebbf7bacf3db842551cf87
Hugo Pineda <hugoycia@gmail.com>
2021-12-16 12:04:21 +0100

    include growth rate estimation for the spectrophotometer OD data

--- settings.R
+++ settings.R
@@ -16,11 +16,12 @@ p.label.log <- function(.l, base=10) {bquote("log"[.(base)]~.(.l)) }
 
 p.label.time.h <- bquote("Time"~(h))
 p.label.od <- function(.w) { bquote("OD"[.(.w)]) }
-p.label.od.720 <- p.label.od(720)
-p.label.od.730 <- p.label.od(730)
+p.label.od.730 <-bquote("OD"[730])
+p.label.ln.od.730 <-bquote("ln OD"[730])
 p.label.od.ln <- function(.w) { p.label.ln(p.label.od(.w)) }
 p.label.fl <- "Fluorescence (AU)"
 p.label.fl.od <- bquote("Fluorescence"%.%"OD"[730]^-1)
 p.label.norm.fl.od <- bquote("Normalized fluorescence"%.%"OD"[730]^-1)
 p.label.nm <- "Wavelength (nm)"
 p.label.norm.abs <- "Normalized absorbance"
+p.label.mu <- bquote(mu~("h"^-1))
